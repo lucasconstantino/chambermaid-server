1ed03bf4d2f1127df376b011b0962c1ab2ca2a4b
Diogo Vianna <diogoviannaaraujo@icloud.com>
2014-11-22 16:55:59 -0200

    Update UserController.js

--- sails/api/controllers/UserController.js
+++ sails/api/controllers/UserController.js
@@ -7,8 +7,14 @@
 
 module.exports = {
   addDevice: function(req, res) {
-  	sn = req.body.sn;
-  	
-	},
+    sn = req.body.sn;
+    Device.findOneBySerialNumber(sn, function(device) {
+      If (device.owner != null) {
+        //send error said device has a owner already
+      } else {
+        //link device to owner
+      }
+    });
+  },
 };
 
